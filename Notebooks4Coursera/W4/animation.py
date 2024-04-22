c19ebf1c7441d0e016f43aab8f418a509fc8f38e
Parry Choi <parrychoi1109@gmail.com>
2024-04-22 17:34:23 +0200

    Update animation

--- Notebooks4Coursera/W4/animation.py
+++ Notebooks4Coursera/W4/animation.py
@@ -1,4 +1,41 @@
+from progress_bar import ProgressBarHandler
+
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+
+
+def create_animation_homogeneous(local_dict):
+    fig2 = local_dict['fig2']
+    ax3 = local_dict['ax3']
+    up41 = local_dict['up41']
+    up42 = local_dict['up42']
+    
+    lim = local_dict['lim']
+    time = local_dict['time']
+    
+    idisp = local_dict['idisp']
+    nt = local_dict['nt']
+    
+    p_results = local_dict['p_results']
+    seis_results = local_dict['seis_results']
+
+    animation_progress_handler = ProgressBarHandler(nt//idisp + 1, "Creating animation...", remain_after_finish=False)
+
+    def update(n, up41, up42):
+        it = n * idisp
+        
+        up41.set_ydata(seis_results[n])
+        up42.set_data(time[it], seis_results[n][it])
+        
+        ax3.set_title('Time Step (nt) = %d' % it)
+        ax3.imshow(p_results[n], vmin=-lim, vmax=+lim, interpolation="nearest", cmap=plt.cm.RdBu)
+
+        animation_progress_handler(n)
+        
+        return up41, up42
+
+    return animation.FuncAnimation(fig2, update, nt//idisp + 1, fargs=(up41, up42), interval=50)
+
 
 def create_animation_advection_1d_euler_scheme(local_dict):
     unew_results = local_dict['unew_results']
@@ -9,11 +46,15 @@ def create_animation_advection_1d_euler_scheme(local_dict):
     nt = local_dict['nt']
     dt = local_dict['dt']
     title1 = local_dict['title1']
+
+    animation_progress_handler = ProgressBarHandler(nt//idisp + 1, "Creating animation...", remain_after_finish=False)
     
     def update(n, line):
         line.set_ydata(unew_results[n])
         ax1.set_title(title1 + ", time step: %i, time: %.2g s" % (n * idisp, n * idisp * dt))
-        
+
+        animation_progress_handler(n)
+                
         return line,
     
     return animation.FuncAnimation(fig1, update, nt//idisp + 1, fargs=(line1, ), interval=50)
@@ -28,11 +69,15 @@ def create_animation_advection_1d_predictor_corrector_scheme(local_dict):
     nt = local_dict['nt']
     dt = local_dict['dt']
     title2 = local_dict['title2']
-    
+
+    animation_progress_handler = ProgressBarHandler(nt//idisp + 1, "Creating animation...", remain_after_finish=False)
+
     def update(n, line):
         line.set_ydata(unew_results[n])
         ax2.set_title(title2 + ", time step: %i, time: %.2g s" % (n * idisp, n * idisp * dt))
-        
+
+        animation_progress_handler(n)
+
         return line,
     
     return animation.FuncAnimation(fig2, update, nt//idisp + 1, fargs=(line2, ), interval=50)
@@ -47,9 +92,13 @@ def create_animation_advection_1d_mccormack_scheme(local_dict):
     dt = local_dict['dt']
     title3 = local_dict['title3']
     
+    animation_progress_handler = ProgressBarHandler(nt//idisp + 1, "Creating animation...", remain_after_finish=False)
+
     def update(n, line):
         line.set_ydata(unew_results[n])
         ax3.set_title(title3 + ", time step: %i, time: %.2g s" % (n * idisp, n * idisp * dt))
+
+        animation_progress_handler(n)
         
         return line,
     
@@ -65,9 +114,13 @@ def create_animation_advection_1d_lax_wendroff_scheme(local_dict):
     dt = local_dict['dt']
     title4 = local_dict['title4']
     
+    animation_progress_handler = ProgressBarHandler(nt//idisp + 1, "Creating animation...", remain_after_finish=False)
+
     def update(n, line):
         line.set_ydata(unew_results[n])
         ax4.set_title(title4 + ", time step: %i, time: %.2g s" % (n * idisp, n * idisp * dt))
+
+        animation_progress_handler(n)
         
         return line,
     
