b7fa7b7804d5162fa1a40e926e533264ead32cd7
Parry Choi <parrychoi1109@gmail.com>
2024-04-22 17:32:46 +0200

    Progress Bar update - Optional title - Remain after ending - Timed refresh rate

--- Notebooks4Coursera/W4/progress_bar.py
+++ Notebooks4Coursera/W4/progress_bar.py
@@ -5,12 +5,14 @@ from IPython.display import clear_output
 
 class ProgressBarHandler:
     
-    def __init__(self, end_tick: int, zero_starting: bool = True, bar_length: int = 20) -> None:
+    def __init__(self, end_tick: int, title: str = None, zero_starting: bool = True, bar_length: int = 20, remain_after_finish: bool = True) -> None:
         self.end_tick = end_tick
         self.zero_starting = zero_starting
         self.bar_length = bar_length
-        
-        self.last_time = time.time()
+        self.title = title
+        self.remain_after_finish = remain_after_finish
+
+        self.last_time = None
         self.last_tick = 0
         
     def __call__(self, current_tick: int, *args: Any, **kwds: Any) -> Any:
@@ -18,15 +20,31 @@ class ProgressBarHandler:
             current_tick += 1
             
         if current_tick >= self.end_tick:
+            if not self.remain_after_finish:
+                clear_output(wait=True)
+                return
+            
             line = ('[{}] {:.2f}% {}/{} '+' '*10).format('█' * self.bar_length, 100, self.end_tick, self.end_tick)
+            if self.title:
+                line = self.title + '\n' + line
             clear_output(wait=True)
+            time.sleep(0.1)
             sys.stdout.write(line)
             sys.stdout.flush()
             print()
             return
-            
+
+        if self.last_time is None:
+            self.last_time = time.time()
+            self.last_tick = current_tick
+            return
+        
         current_time = time.time()
         time_per_tick = (current_time - self.last_time) / (current_tick - self.last_tick) if current_tick > self.last_tick else 0
+
+        if time_per_tick * (current_tick - self.last_tick) < 0.1:
+            return
+        
         self.last_time = current_time
         self.last_tick = current_tick       
         
@@ -37,13 +55,15 @@ class ProgressBarHandler:
                 
         line = '[{}] {:.2f}% {}/{} {}'.format(progress_bar, percentage, min(current_tick, self.end_tick), self.end_tick, time.strftime("%H:%M:%S", time.gmtime(etr)))
         clear_output(wait=False)
+        if self.title:
+            line = self.title + '\n' + line
         sys.stdout.write(line)
         sys.stdout.flush()
         
 
 if __name__ == '__main__':
-    bar_handler = ProgressBarHandler(100)
+    bar_handler = ProgressBarHandler(int(1e6))
     
-    for i in range(100):
+    for i in range(int(1e6)):
         bar_handler(i)
-        time.sleep(0.1)
+        time.sleep(1e-5)
